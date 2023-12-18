20b405fc11ae11a4a3650477c4d5dddd306c0c24
areejtechcampus <areej262626@gmail.com>
2023-12-18 11:39:06 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - puts numbers from 0 to 9
+ *
+ * Return: Always Success (0)
+ */
+int main(void)
+{
+int number;
+for (number = 0; number < 10; number++)
+{
+putchar(number + '0');
+}
+putchar('\n');
+return 0;
+}
