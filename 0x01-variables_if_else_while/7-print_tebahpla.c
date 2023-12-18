4c0f2ec05538e7a3f5c0dbdc446844d01b11fac5
areejtechcampus <areej262626@gmail.com>
2023-12-18 11:50:10 -0800

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - 
+ *
+ * Return: Always Success (0)
+ */
+int main(void)
+{
+int letter;
+for (letter = 100; letter >= 97; letter--)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
