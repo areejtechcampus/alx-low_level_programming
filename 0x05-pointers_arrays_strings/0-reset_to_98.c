830378e3dcad4010f96e675adb2334b827023cc7
areejtechcampus <areej262626@gmail.com>
2024-01-03 01:10:50 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,8 @@
 
 /**
  * main - write pointer to int and update the value
+ * reset_to_98 - Updates the value to 98
+ * @n: Pointer to the integer value to be updated
  *
  * Return: Always 0.
  */
