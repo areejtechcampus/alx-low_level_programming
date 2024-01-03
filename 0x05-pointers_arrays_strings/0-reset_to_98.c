941409f746b9196fccbc6ee1914817f4fda1e592
areejtechcampus <areej262626@gmail.com>
2024-01-03 01:30:00 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 
 /**
- * main - write pointer to int and update tihe value
  * reset_to_98 - Updates the value to 98
  * @n: Pointer to the integer value to be updated
  *
- * Return: Always 0.
+ * Description: This function takes a pointer to an integer and updates to 98
  */
 void reset_to_98(int *n)
 {
