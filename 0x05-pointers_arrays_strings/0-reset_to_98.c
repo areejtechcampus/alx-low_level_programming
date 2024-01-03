c0ae86fdc65874110cf09daf580e28df9170bc83
areejtechcampus <areej262626@gmail.com>
2024-01-03 01:00:35 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,13 +6,7 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void updateValue(int* ptr)
 {
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    &n = 98;
-    printf("n=%d\n", n);
-    return (0);
+    *ptr = 98;
 }
