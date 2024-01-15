ecaf0ff85305c479e8dd50976471add26c8ed217
areejtechcampus <areej262626@gmail.com>
2024-01-15 09:07:13 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * _puts_recursion - Prints a string followed by a new line using recursion.
- * @s - this is string to be printed
+ * @s: this is string to be printed
  */
 void _puts_recursion(char *s)
 {
